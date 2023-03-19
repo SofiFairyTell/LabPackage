6e6bc64e289d841d0e51f51ab91f8c74c0ca346a
SKurbatova <58175476+SofiFairyTell@users.noreply.github.com>
2023-03-19 18:56:25 +0300

    upd

--- src/LabFunctions/Main.java
+++ src/LabFunctions/Main.java
@@ -1,8 +1,11 @@
 package LabFunctions;
 
+import LabFunctions.lab1.ThreeNumbers;
+
 public class Main {
     public static void main(String[] args) {
         System.out.print("Лабораторные функции загружены!");
+        ThreeNumbers.MenuCheckNumbers();
     }
 
 
