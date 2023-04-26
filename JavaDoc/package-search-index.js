098ffd7b94761d33e1ba38121ec896002c9e9482
SKurbatova <58175476+SofiFairyTell@users.noreply.github.com>
2023-04-26 11:05:26 +0300

    regen javadoc

--- JavaDoc/package-search-index.js
+++ JavaDoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"LabFunctions"},{"l":"LabFunctions.lab1"},{"l":"LabFunctions.lab2"},{"l":"LabFunctions.lab3"},{"l":"LabFunctions.lab4"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"LabFunctions"},{"l":"LabFunctions.lab1"},{"l":"LabFunctions.lab2"},{"l":"LabFunctions.lab3"},{"l":"LabFunctions.lab4"},{"l":"LabFunctions.lab5"}];updateSearchResults();
